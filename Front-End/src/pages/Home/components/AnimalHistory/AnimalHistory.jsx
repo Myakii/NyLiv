23de50e5ac46af2e5308885bb0a6713518433050
CapriceSeum <capriceseum@gmail.com>
2023-12-14 12:19:52 +0100

    also change fontawesome to material ui

--- Front-End/src/pages/Home/components/AnimalHistory/AnimalHistory.jsx
+++ Front-End/src/pages/Home/components/AnimalHistory/AnimalHistory.jsx
@@ -4,6 +4,7 @@ import './AnimalHistory.css'
 export default function AnimalHistory() {
   return (
     <section className='animal-history centered-padding-div'>
+      
         <h3 className='text-center'>Nos sourires du moment</h3>
 
       <div className='animal-history-img'>img</div>
