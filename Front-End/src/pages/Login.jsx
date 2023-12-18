4f0b6c2e1a524176f1f034a05483d32e8360b853
CapriceSeum <118892109+CapriceSeum@users.noreply.github.com>
2023-12-18 14:36:44 +0100

    Login page done

--- Front-End/src/pages/Login.jsx
+++ Front-End/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.css'
 
 export default function Login() {
 
@@ -39,24 +40,66 @@ export default function Login() {
 
   }
 
-
+  // C'EST NORMAL QUE CA SCROLL JUSQU'EN BAS C'EST A CAUSE DU DEV TOOL
   
   return (
     <div className='login'>
 
-      {/* <form>
-        <label htmlFor='login-email' >E-mail</label>
-        <input type='email' id='login-email' name='email' onChange={handleEmail} autoComplete='email' />
+      <img className='login-img'src='./Assets/Animals/loginDoggo.png' />
 
-        <label htmlFor='login-password'>Mot de Passe</label>
-        <input type='password' id='login-password' name='password' onChange={handlePassword} />
 
-        <input type='submit' name='submit' onClick={register} />
+      <form className='login-form'>
 
-      </form> */}
+        <header className='logo'>
+          <img src='./Assets/Logo/NyLiv_Logo.png' />
+          <h1 className='text-[#159DC0]'>Connexion</h1>
+        </header>
+
+        <div className='login-inputs'>
+          <div className='input-mail'>
+            <label htmlFor='login-email' ><h3 className='text-[#159DC0]'>Email</h3></label>
+            <input type='email' id='login-email' name='email' onChange={handleEmail} autoComplete='email' />
+          </div>
+
+          <div className='input-password'>
+            <label htmlFor='login-password'><h3 className='text-[#159DC0]'>Mot de Passe</h3></label>
+          
+            <input type='password' id='login-password' name='password' onChange={handlePassword} />
+          </div> 
+
+        </div>
+        
+        <div className='bottom-section'>
+          <nav className='nav-items'>
+            <ul>
+
+              <li>
+                
+                <Link to='/'>
+                <h4 className='text-[#159DC0] text-right'>Mot de passe oublié ?</h4>
+                </Link>
+
+              </li>
+
+              <li>
+
+                <h4 className='flex gap-2 justify-end'>
+                  Pas encore inscrit ? 
+                  <Link to='/inscription'>
+                    <h4 className='text-[#159DC0]'> Rejoignez-nous</h4>
+                  </Link>  
+                </h4>
+
+              </li>
+
+            </ul>
+          </nav>
+          <button className='btn btn-orange ml-auto' type='submit' name='submit' onClick={register}>Se connecter</button>
+
+        </div>
+      </form>
 
       
-      <img src='./Assets/Animals/loginDoggo.png' />
 
 
 
