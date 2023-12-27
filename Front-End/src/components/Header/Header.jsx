8d15c560991ba0ba4b6e367f01a15bbdc4c0aea0
CapriceSeum <118892109+CapriceSeum@users.noreply.github.com>
2023-12-27 15:54:06 +0100

    fix header.jsx

--- Front-End/src/components/Header/Header.jsx
+++ Front-End/src/components/Header/Header.jsx
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import "./Header.css";
-import { authContext, useAuth } from "../../Auth";
-import MenuIcon from "@mui/icons-material/Menu";
-
-export default function Navbar() {
-  const auth = useAuth(authContext);
-=======
 import React, { useEffect, useState } from 'react';
 import './Header.css';
 import { authContext, useAuth } from '../../Auth';
@@ -17,7 +7,8 @@ import AccountHeader from './components/AccountHeader/AccountHeader.jsx';
 
 
 export default function Header() {
->>>>>>> ed6a9a3f1788d32f3af5dceabafacc1c09f77d69
+
+  const auth = useAuth(authContext);
 
   const [user, setUser] = useState({});
 
@@ -28,59 +19,15 @@ export default function Header() {
       setIsLoggedIn(true);
       setUser(auth);
     }
-  }),
-    [auth];
+  }), [auth];
 
   return (
-<<<<<<< HEAD
-    <header className="header centered-padding-div">
-      <nav className="navbar-left">
-        <ul className="nav-items">
-          <li className="nav-item">
-            <Link to="/">
-              <h3>J'adopte</h3>
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link to="/">
-              <h3>Je signale</h3>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <div className="logo">
-        <Link to="/">
-          <img
-            className="navbar-nyliv-logo w-52"
-            src="/Assets/Logo/NyLiv_Logo.png"
-            alt="Logo de NyLiv"
-          />
-        </Link>
-      </div>
-
-      <nav className="navbar-right">
-        <ul className="nav-items">
-          <li className="nav-item">
-            <Link to="/">
-              <h3>Accompagnez-nous</h3>
-            </Link>
-          </li>
-=======
     <header className="header">
 
         <Navbar />
 
         <AccountHeader />
-        
->>>>>>> ed6a9a3f1788d32f3af5dceabafacc1c09f77d69
-
-          <li className="nav-item">
-            <MenuIcon sx={{ fontSize: 60 }} />
-          </li>
-        </ul>
-      </nav>
+      
     </header>
   );
 }
