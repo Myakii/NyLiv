85c38f898cbc278a5a2119fb5280384bfa968716
Myakii <lili_yang@hotmail.fr>
2024-01-12 22:13:52 +0100

    new ReportForm

--- Front-End/src/pages/Report/components/ReportHeader/ReportHeader.jsx
+++ Front-End/src/pages/Report/components/ReportHeader/ReportHeader.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './ReportHeader.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./ReportHeader.css";
+import ReportForm from "../ReportForm/ReportForm";
 
 export default function ReportHeader() {
-
   return (
-    <header className='centered-marg-div'>
+    <header className="centered-marg-div">
+      <h1 className="text-center">Je Signale</h1>
 
-        <h1 className='text-center'>Je Signale</h1>
+      <h3 className="text-center">
+        Bienvenue dans la section "Je Signale" de notre site,
+      </h3>
+      <h3 className="text-center">
+        dédiée à la protection et au bien-être des animaux.
+      </h3>
 
-        <h3 className='text-center'>
-            Bienvenue dans la section "Je Signale" de notre site, 
-        </h3>
-        <h3 className='text-center'>
-            dédiée à la protection et au bien-être des animaux. 
-        </h3>
+      <h4 className="text-center pt-6">
+        Nous croyons fermement que chaque être vivant mérite d'être traité avec
+        respect et compassion. Cette section a été créée dans le but de
+        permettre à nos visiteurs de signaler tout comportement abusif envers
+        les animaux, car votre vigilance est cruciale pour assurer leur
+        sécurité.
+      </h4>
 
-        <h4 className='text-center pt-6'>
-            Nous croyons fermement que chaque être vivant mérite d'être traité
-            avec respect et compassion. 
-            Cette section a été créée dans le but de permettre à nos visiteurs 
-            de signaler tout comportement abusif envers les animaux, 
-            car votre vigilance est cruciale pour assurer leur sécurité.
-        </h4>
-
-        <Link to="/">
-            <button className='btn-blue btn report-button'>
-                Je souhaite signaler
-            </button>
-        </Link>
-
+      <Link to="../ReportForm">
+        <button className="btn-blue btn report-button">
+          Je souhaite signaler
+        </button>
+      </Link>
     </header>
-  )
+  );
 }
